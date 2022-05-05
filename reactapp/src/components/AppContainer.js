ffcfc04f43451bebf9fb90614bfbcf350f460d1b
Kiran Desktop <kiran@gershenfeld.org>
2022-05-05 11:39:43 -0700

    Calendar working

--- reactapp/src/components/AppContainer.js
+++ reactapp/src/components/AppContainer.js
@@ -2,45 +2,83 @@ import React from 'react';
 import axios from 'axios';
 import '../css/styles.css'
 import CalendarD3 from './CalendarD3';
+import * as moment from "moment"
+
+import { Navbar, Nav, NavItem, NavDropdown, MenuItem, Container } from 'react-bootstrap';
+import Select from 'react-select'
 
 
 export default class AppContainer extends React.Component {
     state = {
-        data: [
-          {"x": 0, "y": 0},
-          {"x": 1, "y": 1},
-          {"x": 2, "y": 2},
-          {"x": 3, "y": 3},
-          {"x": 4, "y": 4},
-          {"x": 5, "y": 5}
-        ],
+        data: [],
         dimensions: {
-          "height": 500,
+          "height": 700,
           "width": 500,
           "margin": {
-            "left": 30,
-            "right": 10,
+            "left": 0,
+            "right": 0,
             "top": 10,
             "bottom": 10
           }
-        }
+        },
+        month: ""
     };
     
     componentDidMount() {
-        console.log("Mounting")
-        // axios.get('api/all_livestreams')
-        //     .then(res => {
-        //         this.setState({data: res.data});
-        //     });
-        console.log("AppContainer component data, dimensions:")
-        console.log(this.state.data)
-        console.log(this.state.dimensions)
+      var date = new Date()
+      var firstDay = new Date(date.getFullYear(), date.getMonth() -1, 1);
+      var lastDay = new Date(date.getFullYear(), date.getMonth(), 0);
+        axios.get('api/all_livestreams', { 
+            params: {
+               min_date_inclusive: firstDay.toISOString(),
+               max_date_exclusive:  lastDay.toISOString(),
+            }
+          },{
+             headers: {
+              'Content-Type': 'application/json'
+            }
+          })
+            .then(res => {
+              var data = []
+              for (const [iso_string, hours] of Object.entries(res.data)) {
+                data.push({"date": moment(iso_string), "streamed_hours": hours})
+              }
+              console.log(data)
+              this.setState({data: data});
+            });
+
+      this.state.month = moment().format("MM") 
+      this.state.month = "04"
+      this.state.dimensions.width = document.getElementById("chart-container").offsetWidth
+
     }
   
     render() {
       return (
-        <div className="chart-container">
-            <CalendarD3 data={this.state.data} dimensions={this.state.dimensions} />
+        <div>
+          <div className="nav-bar-container">
+            <Navbar className="justify-content-center" bg="dark" expand="lg" variant="dark">
+                <Navbar.Brand href="#home">MOGUL METRICS</Navbar.Brand>
+            </Navbar>
+          </div>
+          <div className="calendar-section">
+            <div className="month-dropdown">
+              <Dropdown>
+                <Dropdown.Toggle variant="success" id="dropdown-basic">
+                  Month
+                </Dropdown.Toggle>
+
+                <Dropdown.Menu>
+                  <Dropdown.Item href="#/action-1">May 2022</Dropdown.Item>
+                  <Dropdown.Item href="#/action-2">April 2022</Dropdown.Item>
+                  <Dropdown.Item href="#/action-3">March 2022</Dropdown.Item>
+                </Dropdown.Menu>
+              </Dropdown>
+            </div>
+            <div className="chart-container" id="chart-container">
+                <CalendarD3 data={this.state.data} month={this.state.month} year={2022} dimensions={this.state.dimensions} />
+            </div>
+          </div>
         </div>
       );
     }
--- reactapp/src/components/CalendarD3.js
+++ reactapp/src/components/CalendarD3.js
@@ -1,57 +1,120 @@
 import React from 'react';
 import axios from 'axios';
 import * as d3 from "d3";
+import * as d3_chromatic from "d3-scale-chromatic"
 import '../css/styles.css'
+import * as moment from "moment"
 
+/*
+This component must take in a list of objects each of which contains a date and a value for hours streamed, the month to draw, and the dimensions of the desired calendar.
+NOTE* There is a disconnect between the dates of the data given and which month is being drawn. For instance if the data contains only days from february and a few from august
+what month should the calendar draw. In order for the result to make sense *most* of the time I will set it up to draw the month given as a parameter and look for dates within 
+that month range. The calendar will then draw the month with any valid dates given (hopefully all matching) greying out dates that have not occured yet. 
+*/
+function getFirstDayOfMonth(month, year){
+  return moment(year + "-" + month + "-01T00:00:00.000Z")
+}
 
-const CalendarD3 = ({ data, dimensions }) => {
+function daysInMonth (month, year) {
+  return new Date(year, month, 0).getDate();
+}
+
+const CalendarD3 = ({ data, month, year, dimensions }) => {
+    console.log("Calling Calendar")
 
     const svgRef = React.useRef(null);
-    const { width, height, margin } = dimensions;
+    var { width, height, margin } = dimensions;
     const svgWidth = width + margin.left + margin.right;
     const svgHeight = height + margin.top + margin.bottom;
 
-    const cellSize = 15
-    const yearHeight = cellSize * 7 + 25
-    const formatDay = d => ["Mo", "Tu", "We", "Th", "Fr", "Sa", "Su"][d.getUTCDay()]
-    const countDay = d => d.getUTCDay()
+    const day_cols = {"Sunday": 0, "Monday": 1, "Tuesday": 2, "Wednesday": 3, "Thursday": 4, "Friday": 5,"Saturday": 6}
+
+    const first_day = getFirstDayOfMonth(month, year)
+    const number_days_in_month = daysInMonth(month, year)
+    const days_per_row = 7
+    const grid_margin = 10
+    const cellSize = ((width - (6* grid_margin)) / 7) - 1
+    // const formatDay = d => ["Mo", "Tu", "We", "Th", "Fr", "Sa", "Su"][d.getUTCDay()]
+    // const countDay = d => d.getUTCDay()
     const timeWeek = d3.utcSunday
 
-    React.useEffect(() => {
-      console.log("React use effect")
-      const x = d3.scaleLinear()
-        .domain([0, 10])
-        .range([0, width])
-      const y = d3.scaleLinear()
-        .domain([0, 10])
-        .range([height, 0])
-      
-      const svgEl = d3.select(svgRef.current);
-      svgEl.selectAll("*").remove();
+    //Create data for calendar, this array must be the number of days in the specified month. Objects in this array are tagged with a "valid" key to determine if data was actually specified for that date.
+    var calendar_data = []
+    var date_iterator = first_day
+    var data_index = 0
+    var max_hours_streamed = 0
 
-      const svg = svgEl
-        .append("g")
-        .attr("transform", `translate(${margin.left},${margin.top})`);
+    var first_day_offset = (first_day.day() + 1) % 7
 
-      const xAxis = d3.axisBottom(x)
-        .ticks(5)
+    for(var i = 0; i < number_days_in_month + first_day_offset; i++)
+    {
+      var day_obj = {}
+      var column_index = i % days_per_row
+      var row_index = Math.floor(i / days_per_row)
+
+      day_obj["x"] = column_index * (cellSize + grid_margin)
+      day_obj["y"] = row_index * (cellSize + grid_margin)
+
+      if(i < first_day_offset)
+      {
+        day_obj["in_month"] = false
+        calendar_data.push(day_obj)
+        continue
+      }
+
+      day_obj["in_month"] = true
+
+      if(data_index < data.length && data[data_index]["date"].isSame(date_iterator, "day"))
+      {
+        day_obj["valid"] = true
+        day_obj["date"] = data[data_index]["date"]
+        day_obj["streamed_hours"] = data[data_index]["streamed_hours"]
+        if(day_obj["streamed_hours"] > max_hours_streamed)
+        {
+          max_hours_streamed = day_obj["streamed_hours"]
+        }
+
+        data_index++
         
-      svg.append('g').call(xAxis);
+      }else
+      {
+        day_obj["valid"] = false
+        day_obj["date"] = date_iterator
+        day_obj["streamed_hours"] = 0
+      }
 
-      const yAxis = d3.axisLeft(y)
-        .ticks(5)
+      calendar_data.push(day_obj)
 
-      svg.append('g').call(yAxis);
+      date_iterator = date_iterator.add("1", "days")
+    }
 
-      svg.selectAll(".dot")
-        .data(data)
+    React.useEffect(() => {
+
+
+      const svgEl = d3.select(svgRef.current);
+      svgEl.selectAll("*").remove(); // Clear svg content before adding new elements 
+      const svg = svgEl
+        .attr("width", width)
+        .attr("height", height)
+        .append("g")
+         .attr("transform", `translate(${margin.left},${margin.top})`);
+
+      const colorScale = d3.scaleSequential(d3_chromatic.interpolateGreens)
+        .domain([0, max_hours_streamed + 2])
+
+      //Creating a grid of objects in d3
+      svg.selectAll(".day")
+        .data(calendar_data)
         .enter()
-          .append("circle")
-          .attr("fill", "black")
-          .attr("r", 4)
-          .attr("cx", (d)=> x(d.x))
-          .attr("cy", (d)=> x(d.y))
-
+          .append("rect")
+          .attr("width", cellSize)
+          .attr("height", cellSize)
+          .attr("x", (d) => d.x)
+          .attr("y", (d) => d.y)
+          .attr("fill", function(d){
+            if(d.valid){return colorScale(d["streamed_hours"])}
+            else{return "#ebebeb"}
+          })
     }, [data]);
   
     return <svg ref={svgRef} width={svgWidth} height={svgHeight} />;
