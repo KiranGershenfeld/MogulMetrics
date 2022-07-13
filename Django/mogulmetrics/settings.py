47df9909810bac14c2d2e5d856a9d4dc2f12d516
Kiran Gershenfeld <kiran@gershenfeld.org>
2022-07-13 15:16:09 -0700

    adjusted path for cors whitelisting

--- Django/mogulmetrics/settings.py
+++ Django/mogulmetrics/settings.py
@@ -149,7 +149,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 CORS_ORIGIN_WHITELIST = [
     'http://localhost:3000',
     'http://localhost:8000',
-    'https://earnest-crostata-9ec741.netlify.app/',
+    'https://earnest-crostata-9ec741.netlify.app',
 ]
 
 CORS_ALLOWED_ORIGINS = [
