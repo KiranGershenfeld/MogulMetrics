b5c528f8002a07e874de7e3d1418da4256e1950f
Kiran Gershenfeld <kiran@gershenfeld.org>
2022-07-15 14:38:46 -0700

    Updated title and favicon

--- reactapp/src/App.js
+++ reactapp/src/App.js
@@ -5,6 +5,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <title>Mogul Metrics</title>
         <div className="page-content">
           <AppContainer/>
         </div>
