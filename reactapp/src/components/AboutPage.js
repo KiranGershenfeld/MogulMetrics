02e6b95dc9a5c5dd934e496374e22004a7ec347b
Kiran Gershenfeld <kiran@gershenfeld.org>
2022-09-01 17:41:47 -0700

    changed about page

--- reactapp/src/components/AboutPage.js
+++ reactapp/src/components/AboutPage.js
@@ -9,19 +9,39 @@ const AboutPage = () => {
         <div>
             <MogulNavBar />
             <div style={{margin: "auto", marginTop: "25px", width: "40%", textAlign:"center"}}>
-                <div>This website is a visual dashboard Mogul Moves related metrics
-                    <br></br>
-                    <br></br>
-                    The data here is collected with in-house scrapes and databases, without reliance on the YouTube API. The video view data shown on the <a href="/videolifecycle" target='_blank'>Video View Lifecycle</a> page provides a window into how videos are recommended and viewed over time. 
-                    <br></br>
-                    <br></br>
-                    Mogul Metrics is an <a href='https://github.com/KiranGershenfeld/MogulMetrics' target='_blank'>open source</a> project mainly built by <a href='https://www.linkedin.com/in/kirangershenfeld/' target='_blank'>Kiran Gershenfeld</a>. Check out my other projects including the popular <a href='https://www.reddit.com/r/LivestreamFail/comments/mu8nyp/twitch_atlas_vol_3/' target='_blank'>Twitch</a> and <a href='https://youtubeatlas.com' target='_blank'>YouTube</a> Atlas maps on my <a href="https://github.com/KiranGershenfeld">Github</a>! Sorry for any usability issues, web development is not my strong suit.  
-                    <br></br>
-                    <br></br>
-                    The world of content creation is regulated by data driven algorithms and I feel that understanding this data can be a tool for consistent growth across platforms.
-                    <br></br>
-                    <br></br>
+                <div>
+                    <span className='about-heading'>About</span>
+                    <div className='about-spacer-small'></div>
+                    This website is a visual dashboard Mogul Moves related metrics
+                    <div className='about-spacer'></div>
+                    The livestream calendar and streams table are built by checking YouTube every 10 minutes via a combination of API calls and webscraping. The video lifecycle page listens for uploaded videos and checks their viewcounts over time, showing how the algorithm is recommending content.   
+                    <div className='about-spacer'></div>
+                    Mogul Metrics is an <a href='https://github.com/KiranGershenfeld/MogulMetrics' target='_blank'>open source</a> project mainly built by <a href='https://www.linkedin.com/in/kirangershenfeld/' target='_blank'> me, Kiran</a>. Check out my other projects including the popular <a href='https://www.reddit.com/r/LivestreamFail/comments/mu8nyp/twitch_atlas_vol_3/' target='_blank'>Twitch</a> and <a href='https://youtubeatlas.com' target='_blank'>YouTube</a> Atlas maps on my <a href="https://github.com/KiranGershenfeld">Github</a>! Sorry for any usability issues, web development is not my strong suit. 
+                    <div className='about-spacer'></div>
+                    In general I think understanding data can help content creators grow and understand their audience. This dashboard, alongside my other projects, aims to increase that understanding. 
+                    <div className='about-spacer'></div>
+                    If you'd like to contribute or talk about these projects please reach out!
                     Contact me at <a href='mailto:kiran@gershenfeld.org' target='_blank'>kiran@gershenfeld.org</a> with any inquiries, questions, requests, or concerns
+                    <div className='about-spacer'></div>
+                    <div className='about-spacer'></div>
+                    <span className='about-heading'>Tech Stack</span>
+                    <div className='about-spacer-small'></div>
+                    <span className='about-heading-small'>Backend</span>
+                    <div className='about-spacer-small'></div>
+                    The backend for this dashboard is built as a Django REST API written in python and hosted on an EC2 instance. The database uses PostgreSQL on CockroachDB as they provide a generous free tier (cloud hosted on AWS). 
+                    <div className='about-spacer'></div>
+                    The data for the livestreams page is collected with an AWS Lambda function that runs every 10 minutes. This function uses webscraping to check if the channel is live and if so uses the YouTube API to collect information about the stream. This two step process is done to save on quota usage for the API. The video lifecycle data is retrieved on a seperate EC2 instance that is hosting a flask server subscribed to several youtube channels via PubSubHubub. When a new video is published a series of tasks begins to query that video for views according to a predefined schedule.
+                    <div className='about-spacer'></div>
+                    <span className='about-heading-small'>Frontend</span>
+                    <div className='about-spacer-small'></div>
+                    The frontend is built in React.js and deployed using Netlify. The graphs are created in D3.js. The styling is a real blend of mui, basic css, and whatever pre-built elements I could find. I'm very much still learning frontend so its not the prettiest but a webapp felt like the best solution (Unless someone wants to shell out for a Tableau license)
+
+
+
+
+
+                    <div className='about-spacer'></div>
+
                 </div>
             </div>
         </div>);
