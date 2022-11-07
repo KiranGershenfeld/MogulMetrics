018ff60f0e3eade260da9da9c777ea06f7b86d99
Kiran Gershenfeld <kiran@gershenfeld.org>
2022-11-06 19:51:35 -0700

    Added rate limiting to REST API

--- Django/mogulmetrics/settings.py
+++ Django/mogulmetrics/settings.py
@@ -156,3 +156,14 @@ CORS_ALLOWED_ORIGINS = [
     'https://earnest-crostata-9ec741.netlify.app',
     'http://localhost:3000',
 ]
+
+REST_FRAMEWORK = {
+    'DEFAULT_THROTTLE_CLASSES': [
+        'rest_framework.throttling.AnonRateThrottle',
+        'rest_framework.throttling.UserRateThrottle'
+    ],
+    'DEFAULT_THROTTLE_RATES': {
+        'anon': '1000/day',
+        'user': '1000/day'
+    }
+}
