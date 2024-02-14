41270463793a09656401433474acc5d9facf51ab
Kiran Gershenfeld <kiran@gershenfeld.org>
2024-02-13 21:31:49 -0800

    cleaning

--- metricsapp/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-
-from . import views
-
-urlpatterns = [
-    path('api/all_livestreams', views.all_livestreams),
-    path('api/video_lifecycle', views.video_lifecycle)
-]
